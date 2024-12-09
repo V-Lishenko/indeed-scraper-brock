f06baa97eb998c41588216d3ffb81d9da9fdb5f8
V-Lishenko <159156526+V-Lishenko@users.noreply.github.com>
2024-12-09 16:13:39 -0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
-const { Actor, RequestList, RequestQueue, CheerioCrawler, ProxyConfiguration, log } = require('apify');
-const urlParse = require('url-parse');
+import { Actor } from 'apify';
+import { CheerioCrawler } from 'crawlee';
+import urlParse from 'url-parse';
 
 function makeUrlFull(href, urlParsed) {
     if (href.startsWith('/')) return urlParsed.origin + href;
@@ -12,9 +13,9 @@ function getIdFromUrl(url) {
 }
 
 const fromStartUrls = async function* (startUrls, name = 'STARTURLS') {
-    const rl = await RequestList.open(name, startUrls);
+    const rl = await Actor.openRequestList(name, startUrls);
     let rq;
-    while (rq = await rl.fetchNextRequest()) {
+    while ((rq = await rl.fetchNextRequest())) {
         yield rq;
     }
 };
@@ -37,9 +38,9 @@ Actor.main(async () => {
     let { maxItems } = input;
 
     if (maxItems > 990) {
-        log.warning(`The limit of items exceeds the maximum allowed value. Max possible number of offers is 990.`);
+        console.log(`The limit of items exceeds the maximum allowed value. Max possible number of offers is 990.`);
     } else if (maxItems === undefined) {
-        log.info(`No maxItems value provided. Setting it to 990 (max).`);
+        console.log(`No maxItems value provided. Setting it to 990 (max).`);
         maxItems = 990;
     }
 
@@ -73,7 +74,7 @@ Actor.main(async () => {
     let itemsCounter = 0;
     let currentPageNumber = 1;
 
-    const requestQueue = await RequestQueue.open();
+    const requestQueue = await Actor.openRequestQueue();
 
     if (Array.isArray(startUrls) && startUrls.length > 0) {
         for await (const req of fromStartUrls(startUrls)) {
@@ -84,11 +85,11 @@ Actor.main(async () => {
             if (req.url.includes('viewjob')) req.userData.label = 'DETAIL';
             if (!req.url.includes('&sort=date')) req.url = `${req.url}&sort=date`;
             await requestQueue.addRequest(req);
-            log.info(`URL added to queue: ${req.url}`);
+            console.log(`URL added to queue: ${req.url}`);
             countryUrl = `https://${req.url.split('https://')[1].split('/')[0]}`;
         }
     } else {
-        log.info(`Running site crawl for country: ${country}, position: ${position}, location: ${location}`);
+        console.log(`Running site crawl for country: ${country}, position: ${position}, location: ${location}`);
         const startUrl = `${countryUrl}/jobs?${position ? `q=${encodeURIComponent(position)}&sort=date` : ''}${location ? `&l=${encodeURIComponent(location)}` : ''}`;
         await requestQueue.addRequest({
             url: startUrl,
@@ -107,31 +108,24 @@ Actor.main(async () => {
         proxyConfigOptions.countryCode = proxyConfiguration.apifyProxyCountry;
     }
 
-    const proxyConfig = new ProxyConfiguration(proxyConfigOptions);
+    const proxyConfig = await Actor.createProxyConfiguration(proxyConfigOptions);
 
     if (Actor.isAtHome() && !proxyConfig) {
         throw new Error('You must use Apify Proxy or custom proxies to run this scraper on the platform!');
     }
 
-    log.info('Starting crawler...');
+    console.log('Starting crawler...');
     const crawler = new CheerioCrawler({
         requestQueue,
-        useSessionPool: true,
-        sessionPoolOptions: {
-            maxPoolSize: 50,
-            sessionOptions: {
-                maxUsageCount: 50,
-            },
-        },
+        proxyConfiguration: proxyConfig,
         maxConcurrency,
         maxRequestRetries: 5,
-        proxyConfiguration: proxyConfig,
         handlePageFunction: async ({ $, request, response }) => {
-            log.info(`Processing ${request.url} with label ${request.userData.label}`);
+            console.log(`Processing ${request.url} with label ${request.userData.label}`);
             // Implement your `handlePageFunction` logic here
         },
     });
 
     await crawler.run();
-    log.info('Crawl finished.');
+    console.log('Crawl finished.');
 });
